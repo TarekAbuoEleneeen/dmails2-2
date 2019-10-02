569b8c29b9cdc310db4ffd7407391b18ae8147b0
tarek <tarek90045@yahoo.com>
2019-10-02 16:43:41 +0200

    validation

--- main.js
+++ main.js
@@ -362,139 +362,176 @@ function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.focus();
 }
-$(() => {
-    $.validator.setDefaults({
-    highlight: function (element) {
-        $(element)
-        .addClass('has-error');
+function openInSameTab(url) {
+    var win = window.open(url);
+    win.focus();
+}
+//   $(() => {
+//     $.validator.setDefaults({
+//     highlight: function (element) {
+//         $(element)
+//         .addClass('has-error');
         
-    },
-        unhighlight: function (element) {
-            $(element)
+//     },
+//         unhighlight: function (element) {
+//             $(element)
                
-            .removeClass('has-error');
-        }
-    }
-    );
-    $.validator.addMethod("lettersonly", function (value, element) {
-        return this.optional(element) || /^[a-z]+$/i.test(value);
-    }, "Letters only please");
-    $("#buissnes").validate({
-        rules:{
-            email:{
-                required:true,
-                email:true
-            },
-            FirmName:{
-                required:true,
-                lettersonly: true
+//             .removeClass('has-error');
+//         }
+//     }
+//     );
+//     $.validator.addMethod("lettersonly", function (value, element) {
+//         return this.optional(element) || /^[a-z]+$/i.test(value);
+//     }, "Letters only please");
+//     $("#buissnes").validate({
+//         rules:{
+//             email:{
+//                 required:true,
+//                 email:true
+//             },
+//             FirmName:{
+//                 required:true,
+//                 lettersonly: true
 
-            },
-            Name:{
-                required:true,
-                lettersonly: true
+//             },
+//             Name:{
+//                 required:true,
+//                 lettersonly: true
 
-            }
-        },
-         messages: {},
-        errorElement: 'div',
-        errorLabelContainer: '.errorTxt'
+//             }
+//         },
+//          messages: {},
+//         errorElement: 'div',
+//         errorLabelContainer: '.errorTxt'
 
-    })
-    $("#buissnes-2").validate({
-        rules:{ 
-            email2:{
-                required:true,
-                email:true
-            },
-            FirmName2:{
-                required:true,
-                lettersonly: true
+//     });
 
-            },
-            Name2:{
-                required:true,
-                lettersonly: true
+//     $("#buissnes-2").validate({
+//         rules:{ 
+//             email2:{
+//                 required:true,
+//                 email:true
+//             },
+//             FirmName2:{
+//                 required:true,
+//                 lettersonly: true
 
-            }
-        },
-        messages: {},
-        errorElement: 'div',
-        errorLabelContainer: '.errorTxt'
+//             },
+//             Name2:{
+//                 required:true,
+//                 lettersonly: true
 
-    })
-    $("#notified").validate({
-        rules:{
-            email3:{
-                required:true,
-                email:true
-            }
+//             }
+//         },
+//         messages: {},
+//         errorElement: 'div',
+//         errorLabelContainer: '.errorTxt'
 
-        },
-        messages: {},
-        errorElement: 'div',
-        errorLabelContainer: '.errorTxt'
+//     })
+//     $("#notified").validate({
+//         rules:{
+//             email3:{
+//                 required:true,
+//                 email:true
+//             }
+
+//         },
+//         messages: {},
+//         errorElement: 'div',
+//         errorLabelContainer: '.errorTxt'
          
-    })
-    $("#contact").validate({
-        rules:{
-            email4:{
-                required:true,
-                email:true
-            },
-               Name4: {
-                required: true,
-                lettersonly: true
-            }
+//     })
+//     $("#contact").validate({
+//         rules:{
+//             email4:{
+//                 required:true,
+//                 email:true
+//             },
+//                Name4: {
+//                 required: true,
+//                 lettersonly: true
+//             }
 
-        },
-        messages: {},
-        errorElement: 'div',
-        errorLabelContainer: '.errorTxt'
-    })
-    $("#blockstack-id").validate({
-        rules:{
-            blockstackid:{
-                required:true,
+//         },
+//         messages: {},
+//         errorElement: 'div',
+//         errorLabelContainer: '.errorTxt'
+//     })
+//     // $("#blockstack-id").validate({
+//     //     rules:{
+//     //         blockstackid:{
+//     //             required:true,
 
-            }
-        }
-    })
-    $("#blockstack-id-mobile").validate({
-        rules:{
-            blockstackid:{
-                required:true,
+//     //         }
+//     //     }
+//     // })
+//     $("#blockstack-id-mobile").validate({
+//         rules:{
+//             blockstackid:{
+//                 required:true,
 
-            }
-        }
-    })
-});  
+//             }
+//         }
+//     })
+// });  
 
-// $('#buissnes').on('submit',function() {
-//     return $('#buissnes').jqxValidator('validate');
-// })
-// $('#buissnes-2').on('submit',function() {
-//     return $('#buissnes-2').jqxValidator('validate');
-// })
-// $('#notified').on('submit',function() {
-//     return $('#notified').jqxValidator('validate');
-// })
-// $('#contact').on('submit',function() {
-//     return $('#contact').jqxValidator('validate');
-// })
-// $('#blockstack-id-mobile').on('submit',function() {
-//     return $('#blockstack-id').jqxValidator('validate');
-// })
+
 
     
   function submitId() {
         var inputVal = document.getElementById("blockstackid").value;
         if (inputVal.includes(".id" || ".id.blockstack" || ".dmail.id")) {
-            alert('Thank you for submitting your id, we will notify you later!')
+            toastr.success('Form Submited Successfully')
         } else {
-            alert("Please write a better id format")
+            toastr.error('Make sure you entered a correct blockstackid')
         }
     }
+    
+  function submitId2() {
+        var inputVal = document.getElementById("E2").value;
+        var inputVale = document.getElementById("E1").value;
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputVal) && inputVale.trim().length > 1 )  {
+            toastr.success('Form Submited Successfully')
+        } 
+        else {
+            toastr.error('Something went wrong')
+        }
+    }
+  function submitId4() {
+
+        var inputVal = document.getElementById("E4").value;
+        var inputVale = document.getElementById("E5").value;
+        var inputValu = document.getElementById("E6").value;
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputVal) && inputVale.trim().length > 1 && inputValu.trim().length > 1){
+            toastr.success('Form Submited Successfully')
+        } 
+        else {
+            toastr.error('Something went wrong')
+        }
+    }
+  function submitId5() {
+
+        var inputVal = document.getElementById("E7").value;
+        var inputVale = document.getElementById("E8").value;
+        var inputValu = document.getElementById("E9").value;
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputVal) && inputVale.trim().length > 1 && inputValu.trim().length > 1){
+            toastr.success('Form Submited Successfully')
+        } 
+        else {
+            toastr.error('Something went wrong')
+        }
+    }
+  function submitId3() {
+        var inputVal22 = document.getElementById("E3").value;
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputVal22))  {
+            toastr.success('Form Submited Successfully')
+        } 
+        else {
+            toastr.error('Something went wrong')
+        }
+    }
+    
+
 
 
 // $.validator.addMethod("pattern", function (value, element, param) {
